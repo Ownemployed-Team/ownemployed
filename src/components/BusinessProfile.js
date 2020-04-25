6e8f6d3451c0aa28bd12cd6170a558c03bce55ad
Mark Richards <mark.alan.richards@gmail.com>
2020-04-25 21:46:34 +0100

    adding content to business profile section (#21)

--- src/components/BusinessProfile.js
+++ src/components/BusinessProfile.js
@@ -5,6 +5,7 @@ import {useParams} from "react-router";
 import {Avatar, Button, Card, Col, Descriptions, List, Row} from "antd";
 import businesses from "../data/businesses.json";
 import users from "../data/users.json";
+import TagsCard from "./TagsCard";
 
 const {Meta} = Card;
 
@@ -17,34 +18,51 @@ const BusinessProfile = () => {
             <div style={{textAlign: 'center'}}>
                 <Row gutter={4}>
                     <Col span={12}>
-                        <Card>Name: {business.name}</Card>
-                        <Card>Owner: {owner.name}</Card>
-                        <Card>Summary: {business.tagline}</Card>
+                        <Card>
+                            <Descriptions title={business.name} column={1}>
+                                <Descriptions.Item label="Owner"><a href={`/user-profile/${owner.id}`}>{owner.name}</a></Descriptions.Item>
+                                <Descriptions.Item label="Summary">{business.tagline}</Descriptions.Item>
+                            </Descriptions>
+                        </Card>
+                        <Card title="Value Proposition">{business.valueProposition}</Card>
+                        <Card><Button>Want to Join?</Button></Card>
+                        <Card><Button>Offer Mentoring</Button></Card>
+                        <Card><Button>Follow</Button></Card>
                     </Col>
                     <Col span={12}>
-                        <Card>Key activities:
-                        </Card>
-                        <Card>
-                            Key resources:
-                        </Card>
-                        <Card>
-                            Value proposition
-                        </Card>
-                        <Card>
-                            Customer relationships:
-                        </Card>
-                        <Card>
-                            Channels:
-                        </Card>
-                        <Card>
-                            Customer segments:
-                        </Card>
-                        <Card>
-                            Cost structure:
-                        </Card>
-                        <Card>
-                            Revenue stream:
-                        </Card>
+                        <TagsCard
+                            title="Key Activities"
+                            tags={business.keyActivities}
+                        />
+                        <TagsCard
+                            title="Key Resources"
+                            tags={business.keyResources}
+                        />
+                        <TagsCard
+                            title="Customer Relationships"
+                            tags={business.customerRelationships}
+                        />
+                        <TagsCard
+                            title="Channels"
+                            tags={business.channels}
+                        />
+                        <TagsCard
+                            title="Customer Segments"
+                            tags={business.customerSegments}
+                        />
+
+                        <TagsCard
+                            title="Cost Structure"
+                            tags={business.costStructure}
+                        />
+                        <TagsCard
+                            title="Revenue Stream"
+                            tags={business.revenueStream}
+                        />
+                        <TagsCard
+                            title="Key Partners"
+                            tags={business.keyPartners}
+                        />
                     </Col>
                 </Row>
             </div>
--- src/components/TagsCard.js
+++ src/components/TagsCard.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import {Card, Tag} from "antd";
 const {Meta} = Card;
-const TagsCard = ({tags, title}) => (
-    <Card>
-        <Meta title={title}/>
-        {Object.values(tags).map((item) => (<Tag>{item}</Tag>))}
-    </Card>
-)
+const TagsCard = ({tags, title}) => {
+    console.log(typeof tags)
+
+    if (!Array.isArray(tags)) {
+        return <div/>
+    }
+    else {
+        return    (
+            <Card>
+                <Meta title={title}/>
+                {Object.values(tags).map((item) => (<Tag>{item}</Tag>))}
+            </Card>
+        )
+    }
+
+}
 
 export default TagsCard
