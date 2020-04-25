77e031532dcd10d83d622bd583516378889fe97f
Mark Richards <mark.alan.richards@gmail.com>
2020-04-25 16:23:57 +0100

    User style evolving

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,31 +1,144 @@
 import * as React from 'react'
-import { Card} from 'antd'
+import {Avatar, Card, Col, List, Row, Typography, Descriptions} from 'antd'
 import PageLayout from './PageLayout'
 import {useParams} from "react-router";
-const { Meta } = Card;
+
+const {Meta} = Card;
 
 export const profiles = {
     deborah: {
         name: "Deborah",
         summary: "Marketing Manager",
+        socialMedia: {
+            "facebook": "https://facebook.com"
+        },
+        ownedModels: [
+            "Project X"
+        ],
+        connectedModels: [
+            "Project Y"
+        ]
+
     },
 }
 
+const socialMediaText = {
+    "facebook": {
+        "name": "Facebook",
+        "connectionType": "Friend"
+    }
+}
+
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    }
+];
+
 const UserProfile = () => {
 
-  const { userProfileId } = useParams()
+    const {userProfileId} = useParams()
     let profile = profiles[userProfileId];
 
     return (<PageLayout>
-      <div style={{ textAlign: 'center'}}>
-          <Card
-              // hoverable
-              style={{ width: 240 }}
-              cover={<img alt="example" src={`/imgs/users/${userProfileId}.png`} />}>
-              <Meta title={profile.name} description={profile.summary} />
-          </Card>,
-      </div>
-  </PageLayout>
-)}
+            <div style={{textAlign: 'center'}}>
+                <Row gutter={4}>
+                    <Col span={12}>
+                        <Card
+                            cover={<img alt={profile.name} src={`/imgs/users/${userProfileId}.png`}/>}>
+                            <Meta title={profile.name} description={profile.summary}/>
+                        </Card>
+                        <Card>
+                            <List
+                                header="Looking for"
+                                dataSource={["Business Networking", "Product Launch"]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        {item}
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+                        <Card>
+                            <Descriptions title="User Info" column={1}>
+                                <Descriptions.Item label="Location">Copenhagon, Denmark</Descriptions.Item>
+                                <Descriptions.Item label="Summary">Experienced Marketing Manager with entrepreneurial
+                                    itch solving problems for flower shops</Descriptions.Item>
+                                <Descriptions.Item label="Education">Msc Logistics and Supply Chain, Aarhus
+                                    University</Descriptions.Item>
+                            </Descriptions>,
+                        </Card>
+                        <Card>
+                            <List
+                                header="Skills"
+                                dataSource={["Brand Management", "Product Launch"]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        {item}
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+
+                        <Card>
+                            <List
+                                header="Interests"
+                                dataSource={["Sustainability", "Blockchain", "Volunteering"]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        {item}
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={12}>
+                        <Card>
+                            <List
+                                header={<div>Business model projects</div>}
+                                itemLayout="horizontal"
+                                dataSource={data}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            avatar={<Avatar
+                                                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                                            title={<a href="https://ant.design">{item.title}</a>}
+                                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                        />
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+                        <Card>
+                            Deborah's activity
+                        </Card>
+                        <Card>
+                            Deborah's activity
+                        </Card>
+
+
+                        <Row gutter={4}>
+                            {Object.keys(profile.socialMedia).map(k =>
+                                <Col span={4}>
+                                    <a href={profile.socialMedia[k]}>
+                                        <img alt={k} src={`/imgs/social-media/${k}.svg`}/></a>
+                                </Col>
+                            )}
+
+                        </Row>
+
+                    </Col>
+
+                </Row>
+
+
+            </div>
+        </PageLayout>
+    )
+}
 
 export default UserProfile
