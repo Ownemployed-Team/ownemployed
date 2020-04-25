c8e337d1b9de26a957dc9e047a72a84b67ae616c
gwens <gwenan.spearing@vimcar.com>
2020-04-25 15:39:38 +0200

    add logo to header

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
+import logo from '../static/ownemployed_logo.png'
 
 const { Header, Content } = Layout
 
 const PageLayout = ({ children }) => (
   <Layout style={{ minHeight: '100vh' }}>
-    <Header style={{ background: '#ffffff' }}>
+    <Header style={{ background: '#ffffff', height: '130px' }}>
+      <div style={{ textAlign: 'center' }}><img alt='logo' src={logo} width='245px'/></div>
       <Menu mode='horizontal' theme='light'>
         <Menu.Item><Link to='/'>Home</Link></Menu.Item>
         <Menu.Item><Link to='/business-profile'>Business Profile</Link></Menu.Item>
