2de5e850ce9a55393a30b4591a2b2d56bf9fd5dc
Mark Richards <mark.alan.richards@gmail.com>
2020-04-26 10:58:30 +0100

    <a> in UserCard (#27)
    * <a> in UserCard
    
    * swap to Link

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Card } from 'antd'
+import {Card} from 'antd'
 import avatar from '../static/avatars/user/deborah.png'
+import {Link} from "react-router-dom";
 
-const { Meta } = Card
-const UserCard = ({ user, style }) => (
-  <Card
-    cover={
-      <div style={{ height: '110px', margin: 'auto', width: '110px' }}>
-        <img alt='member' src={avatar}/>
-      </div>
-    }
-    style={{ height: '300px', width: '300px', margin: '16px', padding: '24px' }}
+const {Meta} = Card
+const UserCard = ({user, style}) => (
+  <Link to={`/user-profile/${user.id}`}>
+    <Card
+      cover={
+        <div style={{height: '110px', margin: 'auto', width: '110px'}}>
+          <img alt='member' src={avatar}/>
+        </div>
+      }
+      style={{height: '300px', width: '300px', margin: '16px', padding: '24px'}}
     >
-    <Meta
-      title={user.name}
-      description={user.summary}
-    />
-  </Card>
+      <Meta
+        title={user.name}
+        description={user.summary}
+      />
+    </Card>
+  </Link>
 )
 
 
