6434dce1566ec29e0a4c2db0c630b33effdd2e39
Gwenan Spearing <gwenanspearing@hotmail.com>
2020-04-26 18:48:43 +0200

    improve landing page styling and add a bit more data to Med Find It (#46)

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,17 @@ import * as React from 'react'
 import { Typography, Row, Col } from 'antd'
 import PageLayout from './PageLayout'
 import LandingPageCard from './LandingPageCard'
+import { colors } from '../utils/colors'
 
 const { Paragraph, Title} = Typography
 
 const Home = () => (
   <PageLayout>
     <div style={{ textAlign: 'center'}}>
-      <Title>Why wait to be employed, when you can be an entrepreneur?</Title>
-      <Paragraph>The covid-19 pandemic has led to record levels of unemployment across the world - with many highly skilled people struggling to find an income. On this platform, you can start planning your business or join other people’s projects.</Paragraph>
-      <Paragraph>Because why look for employment, when you can create it?</Paragraph>
-      <Paragraph>Take the opportunity to start something you are passionate about and eventually make a living out of it!</Paragraph>
+      <Title style={{ color: colors.primaryLight }}>Why wait to be employed, when you can be an entrepreneur?</Title>
+      <Paragraph style={{ fontSize: '18px', fontStyle: 'italic' }}>The covid-19 pandemic has led to record levels of unemployment across the world - with many highly skilled people struggling to find an income. On this platform, you can start planning your business or join other people’s projects.</Paragraph>
+      <Paragraph style={{ fontSize: '18px', fontStyle: 'italic' }}>Because why look for employment, when you can create it?</Paragraph>
+      <Paragraph style={{ fontSize: '18px', fontStyle: 'italic', color: colors.primaryDark, fontWeight: '600' }}>Take the opportunity to start something you are passionate about and eventually make a living out of it!</Paragraph>
       <Row gutter={16}>
         <Col span={8}><LandingPageCard text='I want to start a project' to="/explore-ideas"/></Col>
         <Col span={8}><LandingPageCard text='I want to join others with a business idea' to="/explore-ideas"/></Col>
--- src/components/LandingPageCard.js
+++ src/components/LandingPageCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Typography, Card} from 'antd'
 import {Link} from "react-router-dom";
+import { colors } from '../utils/colors'
 
 const {Paragraph} = Typography
 
@@ -8,12 +9,13 @@ const LandingPageCard = ({text, to}) => (
   <Link to={to}>
     <Card
       bodyStyle={{display: 'flex', alignItems: 'center', height: '300px'}}
-      style={{height: '300px'}}
+      style={{height: '300px', borderRadius: '10px', boxShadow: '4px 2px 10px lightgray', margin: '16px'}}
     >
       <Paragraph
         style={{
           margin: 'auto',
           fontSize: '18px',
+          color: colors.primaryLight,
           fontWeight: '600'
         }}
       >{text}
