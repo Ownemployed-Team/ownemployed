e93e17eeff91f82c9e770a5fea439dc6889a3abb
Mark Richards <mark.alan.richards@gmail.com>
2020-04-26 11:35:44 +0100

    Adding text from slides and updating the landing page cards (#31)

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,19 @@ import { Typography, Row, Col } from 'antd'
 import PageLayout from './PageLayout'
 import LandingPageCard from './LandingPageCard'
 
-const { Paragraph } = Typography
+const { Paragraph, Title} = Typography
 
 const Home = () => (
   <PageLayout>
     <div style={{ textAlign: 'center'}}>
-      <Paragraph>From unemployed to entrepreneur - own your employment.</Paragraph>
-      <Paragraph>We are a networking site helping people like you take control & own your employment.</Paragraph>
-      <Paragraph>Find other budding entrepreneurs & start your new business today!</Paragraph>
+      <Title>Why wait to be employed, when you can be an entrepreneur?</Title>
+      <Paragraph>The covid-19 pandemic has led to record levels of unemployment across the world - with many highly skilled people struggling to find an income. On this platform, you can start planning your business or join other people’s projects.</Paragraph>
+      <Paragraph>Because why look for employment, when you can create it?</Paragraph>
+      <Paragraph>Take the opportunity to start something you are passionate about and eventually make a living out of it!</Paragraph>
       <Row gutter={16}>
-        <Col span={8}><LandingPageCard text='I have a business idea'/></Col>
-        <Col span={8}><LandingPageCard text='I want to join others with a business idea'/></Col>
-        <Col span={8}><LandingPageCard text='I want to help others with their business idea'/></Col>
+        <Col span={8}><LandingPageCard text='I want to start a project' to=""/></Col>
+        <Col span={8}><LandingPageCard text='I want to join others with a business idea' to="/explore-ideas"/></Col>
+        <Col span={8}><LandingPageCard text='I want to help others with their business idea' to=""/></Col>
       </Row>
     </div>
   </PageLayout>
--- src/components/LandingPageCard.js
+++ src/components/LandingPageCard.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Typography, Card } from 'antd'
+import {Typography, Card} from 'antd'
+import {Link} from "react-router-dom";
 
-const { Paragraph } = Typography
+const {Paragraph} = Typography
 
-const LandingPageCard = ({ text }) => (
-  <Card
-    bodyStyle={{ display: 'flex', alignItems: 'center', height: '300px' }}
-    style={{ height: '300px' }}
+const LandingPageCard = ({text, to}) => (
+  <Link to={to}>
+    <Card
+      bodyStyle={{display: 'flex', alignItems: 'center', height: '300px'}}
+      style={{height: '300px'}}
     >
-    <Paragraph
-      style={{
-        margin: 'auto',
-        fontSize: '18px',
-        fontWeight: '600'
-      }}
+      <Paragraph
+        style={{
+          margin: 'auto',
+          fontSize: '18px',
+          fontWeight: '600'
+        }}
       >{text}
-    </Paragraph>
-  </Card>
+      </Paragraph>
+    </Card>
+  </Link>
 )
 
 export default LandingPageCard
