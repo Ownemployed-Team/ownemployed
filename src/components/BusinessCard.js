7062515a7ddad3ba9214d90114fda1617b744254
Gwenan Spearing <gwenanspearing@hotmail.com>
2020-04-26 18:12:55 +0200

    fix business card format and reduce size of avatars (#45)

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Card, Avatar} from 'antd'
-import avatar from '../static/avatars/business/Captain_Planet.png'
 import {Link} from "react-router-dom";
+import { colors } from '../utils/colors'
 
 const { Meta } = Card
 
 const BusinessCard = ({ business, style }) => {
+  let avatar = require('../static/avatars/business/default.png')
+  try {
+      avatar = require(`../static/avatars/business/${business.id}.png`)
+  } catch (e) {}
     return (
       <Link to={`/projects/${business.id}`}>
         <Card style={{ height: '220px', width: '300px', margin: '8px' }} title={business.name}>
@@ -16,17 +20,17 @@ const BusinessCard = ({ business, style }) => {
             />
             <div
             style={{
-                fontSize: '10px',
-                borderTop: '1px solid lightgray',
+                fontSize: '12px',
+                color: colors.primaryLight,
                 padding: '8px 24px',
-                height: '32px',
-                width: '100%',
                 position: 'absolute',
                 bottom: '10px',
                 left: '0px'
                 }}
                 >
-                {business.status || ''}
+                {business.status ? `Status: ${business.status}` : ''}
+                <br/>
+                {business.contributors ? `Contributors: ${business.contributors.toString()}` : ''}
             </div>
         </Card>
       </Link>
