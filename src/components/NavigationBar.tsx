66721a8783c36881cf64da4efd5654bd420acbde
Daniel M <nopestack@users.noreply.github.com>
2020-05-11 21:43:42 +0200

    Update NavigationBar.tsx

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -13,30 +13,6 @@ type NavigationItem = {
     isAuth?: boolean
 }
 
-const MenuItem = ({ item, ...props }) => {
-    const { url, label, isAuth } = item
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
-
-    if (isAuth) {
-        return (
-            <Menu.Item
-                onClick={() => {
-                    isAuthenticated ? logout() : loginWithRedirect({})
-                }}
-                {...props}
-            >
-                {isAuthenticated ? 'Log out' : 'Log in'}
-            </Menu.Item>
-        )
-    }
-
-    return (
-        <Menu.Item {...props}>
-            <Link to={url}>{label}</Link>
-        </Menu.Item>
-    )
-}
-
 const Brand = () => {
     return (
         <Link to="/">
