9b912024db05abfccd36ffa98856e6ef2dc0570d
Chi Hao Poon <karvex@hotmail.com>
2020-05-14 19:07:50 +0200

    Split into separate files.

---
+++ src/graphql/get-users.js
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-boost'
+
+// Getting all users profiles data
+//TODO: Determine which fields we'll need to lighten the query, and change the query in consequence
+export const GET_USERS = gql`
+    {
+        getUsers {
+            id
+            name
+            signupDate
+            socialMedia {
+                name
+                url
+            }
+            interests
+            lookingFor
+            skills
+            education
+            location
+        }
+    }
+`
