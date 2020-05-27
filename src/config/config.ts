c8a1f80538168cfbe9643fa296debd7d4d963196
Daniel M <nopestack@gmail.com>
2020-05-27 20:56:12 +0200

    add cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,21 @@
+const debug = process.env.REACT_APP_DEBUG || true
+const apiURL = process.env.REACT_APP_API_URL || 'localhost:8080/api/v1'
+const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN || ''
+const auth0Audience = process.env.REACT_APP_AUTH0_AUDIENCE || ''
+const auth0ClientID = process.env.REACT_APP_AUTH0_CLIENT_ID || ''
+const cloudinaryCloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME || ''
+const cloudinaryAPIKey = process.env.REACT_APP_CLOUDINARY_API_KEY || ''
+const cloudinaryAPISecret = process.env.REACT_APP_CLOUDINARY_API_SECRET || ''
+const cloudinaryAPIEndpoint = process.env.REACT_APP_CLOUDINARY_API_URL || ''
+
 export default {
-    debug: process.env.REACT_APP_DEBUG,
-    apiURL: process.env.REACT_APP_API_URL,
-    auth0Domain: process.env.REACT_APP_AUTH0_DOMAIN || '',
-    auth0Audience: process.env.REACT_APP_AUTH0_AUDIENCE || '',
-    auth0ClientID: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
+    debug,
+    apiURL,
+    auth0Domain,
+    auth0Audience,
+    auth0ClientID,
+    cloudinaryCloudName,
+    cloudinaryAPIKey,
+    cloudinaryAPISecret,
+    cloudinaryAPIEndpoint,
 }
