733493ce68094608414ef949cd4ffed15553a360
Chi Hao Poon <karvex@hotmail.com>
2020-06-07 17:20:02 +0200

    Fix find members page

--- src/graphql/get-users.js
+++ src/graphql/get-users.js
@@ -3,21 +3,48 @@ import { gql } from 'apollo-boost'
 // Getting all users profiles data
 //TODO: Determine which fields we'll need to lighten the query, and change the query in consequence
 export const GET_USERS = gql`
-    {
-        getUsers {
+    query getUsers(
+        $name: String
+        $location: String
+        $skills: [String]
+        $sectors: [String]
+        $skip: Int
+        $take: Int
+    ) {
+        getUsers(
+            name: $name
+            location: $location
+            skills: $skills
+            sectors: $sectors
+            skip: $skip
+            take: $take
+        ) {
             id
-            name
+            firstName
+            lastName
+            email
             signupDate
+            headline
+            education
+            bio
+            location
+            remote
+            skills {
+                id
+                title
+                category
+            }
+            sectors {
+                id
+                title
+                category
+            }
             socialMedia {
                 name
                 url
             }
+            emailVisibility
             avatar
-            interests
-            lookingFor
-            skills
-            education
-            location
         }
     }
 `
