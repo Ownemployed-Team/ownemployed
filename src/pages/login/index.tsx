0cb60828f8c47b63e132bdc72e04bce7278c5d23
Yatit Thakker <thakkery@gmail.com>
2020-06-12 00:02:11 -0700

    Update naming

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { Image } from 'rebass'
 import Layout from '../../components/Layout'
-import RegForm from './membershipForm'
+import MembershipForm from './membershipForm'
 import './index.css'
 
 interface LoginPageProps {}
@@ -12,7 +12,7 @@ export default class LoginPage extends PureComponent<LoginPageProps> {
         return (
             <Layout boxed={false}>
                 <div className="LoginPage-container">
-                    <RegForm />
+                    <MembershipForm />
                     <Image src="/imgs/illustrations/home.svg" />
                 </div>
             </Layout>
